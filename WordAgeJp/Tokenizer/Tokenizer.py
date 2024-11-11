ddc0e02ff4e9ddd2d9c7c25784a9e510cc031cc9
konbraphat51 <konbraphat@gmail.com>
2024-11-11 17:24:11 +0900

    tokenizing texts

--- WordAgeJp/Tokenizer/Tokenizer.py
+++ WordAgeJp/Tokenizer/Tokenizer.py
@@ -1,12 +1,41 @@
-from Tokenizer.TokenizedData import TokenizedData
+from MeCab import Tagger
+from WordAgeJp.Tokenizer.TokenizedData import TokenizedData
 
 class Tokenizer:
     """
     Tokenizing texts
     """
 
-    def tokenize(text: str) -> TokenizedData:
+    def __init__(self) -> None:
+        self.stopwords = set([
+            "",
+            "。",
+            "、"
+        ])
+
+    def tokenize(self, text: str) -> TokenizedData:
         """
         Tokenize text
         """
-        pass
+
+        tagger = Tagger()
+        tagger.parse('')
+        node = tagger.parseToNode(text)
+        tokenized_data = TokenizedData()
+        while node:
+            surface = node.surface
+
+            # skip stopwords
+            if surface in self.stopwords:
+                node = node.next
+                continue
+
+            tokenized_data.infinitives.append(surface)
+            node = node.next
+            
+        return tokenized_data
+    
+if __name__ == "__main__":
+    text = "私はPythonが好きです。"
+    tokenized_data = Tokenizer().tokenize(text)
+    print(tokenized_data.infinitives)
