f0914e49eec2b0b60fbad055cea964e25d2c0439
konbraphat51 <konbraphat@gmail.com>
2024-11-11 17:59:05 +0900

    prepare rating dictionary

--- WordAgeJp/Evaluator/Evaluator.py
+++ WordAgeJp/Evaluator/Evaluator.py
@@ -10,14 +10,35 @@ class Evaluator:
                 Path(__file__).parent.parent / "Assets" / "goi.csv"
             )
 
-        self.prepare_dictionary(self.data_path)
+        self.ratings = self.get_ratings(self.data_path)
 
-    def prepare_dictionary(self, data_path: str) -> None:
+    def get_ratings(self, data_path: str) -> dict[str, int]:
         """
-        prepare dictionary for evaluation
+        prepare ratings dictionary for evaluation
+
+        :param data_path: path to the csv file
+        :return: dictionary of ratings. key: surface, value: rating
         """
+
+        #read csv
+        with open(data_path, "r", encoding="shift_jis") as f:
+            lines_raw = f.read().splitlines()
+        lines = []
+        for line in lines_raw:
+            lines.append(line.split(","))
+
+        #surface -> rating
+        ratings = {}
+        for line in lines[1:]:
+            # get only difficulty number
+            ratings[line[1]] = int(line[3][0])
+
+        return ratings
 
     def process(self, surfaces: list[str]) -> None:
         """
         receive surfaces data and process them
         """
+
+if __name__ == "__main__":
+    instance = Evaluator()
